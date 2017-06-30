c26fa17ff10ac9943e38df0a5844fcba5b3b2974
cahhansen <carlyahansen@gmail.com>
2017-06-30 14:58:35 -0500

    Make into Function
    added documentation and ability to subset to a list of reaches (COMIDs)

--- NWM_Retro_Analysis/NetCDFProcessing.R
+++ NWM_Retro_Analysis/NetCDFProcessing.R
@@ -1,52 +1,49 @@
 library(ncdf4)
+
 #Set working directory to folder with a year of NetCDF data
 directory=getwd()
 #Set reach (From list of subsetted reaches/gages)
-comid=22548559
+comids=c(22548559,8212073)
 
-hourList=sprintf("%02d",seq(0,23,by=1))
-year=substr(directory,nchar(directory)-4+1, nchar(directory))
-is.leapyear=function(year){
-  return(((year %% 4 == 0) & (year %% 100 != 0)) | (year %% 400 == 0))
-}
-dates=read.csv('C:/Users/carly/Desktop/SummerInstitute/DateList.csv')
-if (is.leapyear(as.numeric(year))){
-  dateList=sprintf("%04d",dates$LeapYear)
-}else{
-  dateList=sprintf("%04d",dates$NonLeapYear) 
-  dateList=dateList[(dateList!="  NA")]
-}
-
-#Cycle through the hourly time steps and convert to daily
-ncdfFileList=shell('dir /b', intern=TRUE)
-start.time = Sys.time()
-dailyQDF=data.frame(Date=dateList)
-for (y in dateList){
-  hourlyQDF=data.frame(Hour=hourList)
-  for (x in hourList){
-    ncdfFileName=paste0(year,y,x,"00_streamflow.nc")
-    ncdfFile=paste0(directory,"/",ncdfFileName)
-    nwmFile=nc_open(ncdfFile,readunlim=FALSE)
-    #Get index of COMID from netcdf (dim=1 is the stream reach)
-    featureIDList=nwmFile$dim$feature_id$vals
-    featureIndex=match(comid,featureIDList)
-    
-    #Get streamflow (in cms)
-    nwmData=ncvar_get(nwmFile,
-                   varid="streamflow",
-                   start=c(featureIndex,1), #Start value for dimensions (first value is stream reach index)
-                   count = c(1,-1)) #Number of values to get in each dimension
-    nc_close(nwmFile)
-    hourlyQDF[(hourlyQDF$Hour==x),"Reach"]=nwmData
+agg_hourly_streamflow = function(directory,comids){
+  year=substr(directory,nchar(directory)-3,nchar(directory))
+  #Create list for cycling through hourly files
+  hourList=sprintf("%02d",seq(0,23,by=1))
+  
+  #Cycle through the hourly time steps and convert to daily
+  ncdfFileList=shell('dir /b', intern=TRUE)
+  dateList=substr(ncdfFileList,5,8)
+  #Initialize data frame for daily summaries
+  dailyQDF=data.frame(Date = dateList)
+  dailyQDF[as.character(featureIndex)]=NA
+  for (y in dateList){
+    #Initialize data frame for hourly values
+    hourlyQDF=data.frame(Hour=hourList)
+    for (x in hourList){
+      #Get and open netcdf files
+      ncdfFileName=paste0(year,y,x,"00_streamflow.nc")
+      ncdfFile=paste0(directory,"/",ncdfFileName)
+      nwmFile=nc_open(ncdfFile,readunlim=FALSE)
+      #Get index of COMID from netcdf (dim=1 is the stream reach)
+      featureIDList=nwmFile$dim$feature_id$vals
+      featureIndex=match(comids,featureIDList)
+      
+      for (i in featureIndex){
+      #Get streamflow for chosen reaches(in cms)
+      nwmData=ncvar_get(nwmFile,
+                     varid="streamflow",
+                     start=c(i,1), #Start value for dimensions (first value is stream reach index)
+                     count = c(1,-1)) #Number of values to get in each dimension
+      #Add hourly streamflow value to data frame
+      hourlyQDF[(hourlyQDF$Hour==x),as.character(i)]=nwmData
+      }
+    }
+    #Calculate daily mean streamflow
+    meanDailyQ=colMeans(hourlyQDF[,2:length(hourlyQDF)],na.rm=TRUE)
+    #Add daily mean streamflow to data frame
+    dailyQDF[(dailyQDF$Date==y),c(as.character(featureIndex))]=meanDailyQ
   }
-  meanDailyQ=mean(hourlyQDF$Reach)
-  dailyQDF[(dailyQDF$Date==y),"Reach"]=meanDailyQ
+  #Output results to csv file
+  write.csv(dailyQDF,file=paste0(directory,"/",year,".csv"),row.names=FALSE)
+  
 }
-names(dailyQDF)[ncol(dailyQDF)]=comid
-
-end.time = Sys.time()
-time.taken = end.time - start.time
-time.taken
-
-write.csv(dailyQDF,file=paste0("C:/Users/carly/Desktop/SummerInstitute/",year,".csv"),row.names=FALSE)
-
