ca50dd5dc367e18b810536ee3319b072d8d27ab3
cahhansen <carlyahansen@gmail.com>
2017-06-30 23:29:41 -0500

    add placeholder features to app

--- tethysapp/low_flows/controllers.py
+++ tethysapp/low_flows/controllers.py
@@ -1,12 +1,60 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from tethys_sdk.gizmos import Button
+import plotly.graph_objs as go
+from tethys_sdk.gizmos import MapView, Button, ToggleSwitch, TextInput, PlotlyView
 
 @login_required()
 def home(request):
     """
     Controller for the app home page.
     """
+    watershed_map = MapView(
+        height='50%',
+        width='50%',
+        layers=[],
+        basemap='OpenStreetMap',
+    )
+
+    forecast_plot = PlotlyView(
+        [go.Scatter(x=[3, 5, 7], y=[1, 3, 6])]
+    )
+
+    load_watershed = Button(
+        name='load-watershed',
+        display_text='Load Watershed'
+    )
+
+    zoom_watershed = Button(
+        name='zoom-watershed',
+        display_text='Zoom to Watershed'
+    )
+
+    show_7q10 = ToggleSwitch(
+        name='toggle7q10',
+        display_text='7Q10'
+    )
+
+    show_25 = ToggleSwitch(
+        name='toggle25',
+        display_text='25th Percentile Flow'
+    )
+
+    custom_amt = TextInput(
+        name='custom_amt',
+        display_text='Enter Custom Amount',
+        placeholder='e.g. 30 cfs'
+    )
+
+    show_wq = ToggleSwitch(
+        name='togglewq',
+        display_text='Real-time Water Quality Data'
+    )
+
+    show_rtq = ToggleSwitch(
+        name='togglertq',
+        display_text='Real-time Streamflow'
+    )
+
     save_button = Button(
         display_text='',
         name='save-button',
@@ -64,6 +112,15 @@ def home(request):
     )
 
     context = {
+        'load_watershed':load_watershed,
+        'zoom_watershed':zoom_watershed,
+        'show_7q10':show_7q10,
+        'show_25':show_25,
+        'custom_amt':custom_amt,
+        'show_wq':show_wq,
+        'show_rtq':show_rtq,
+        'watershed_map': watershed_map,
+        'plotly_view_input':forecast_plot,
         'save_button': save_button,
         'edit_button': edit_button,
         'remove_button': remove_button,
