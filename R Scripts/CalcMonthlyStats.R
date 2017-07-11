3e44bb72b3e3953f1db13077bebdb40d01ee1d03
cahhansen <carlyahansen@gmail.com>
2017-07-11 15:28:10 -0500

    Add functions for calculating monthly stats

---
+++ R Scripts/CalcMonthlyStats.R
@@ -0,0 +1,61 @@
+library(PearsonDS)
+library(dataRetrieval)
+
+
+USGS_Stats = function(gageID){
+  qData = readNWISdata(sites=gageID, service="dv",parameterCd="00060",
+                       startDate="1993-01-01",endDate="2016-10-01")
+  #Calculate monthly statistics
+  qData$Month = months(qData$dateTime)
+  monthlyQStats = data.frame(Month = unique(qData$Month),Min=NA,Avg=NA,Max=NA,Perc5=NA,Perc25=NA,x7Q10=NA,x7Q2=NA)
+  #Read in discharge data
+  for (i in unique(qData$Month)){
+    q = qData[(qData$Month==i),"X_00060_00003"]
+    #Use Log-Pearson Type III Distribution. See: https://pubs.usgs.gov/sir/2008/5126/section3.html and
+    #http://deq1.bse.vt.edu/sifnwiki/index.php/R_iha_7q10#PearsonDS_function_details for the details on this function
+    logq=log(q)
+    pars = PearsonDS:::pearsonIIIfitML(logq)
+    x7Q2 = exp(qpearsonIII(0.5, params = pars$par))
+    x7Q10 = exp(qpearsonIII(0.1, params = pars$par))
+    min = min(q)
+    avg = mean(q)
+    max = max(q)
+    perc5 = quantile(q,na.rm = TRUE, probs=0.05)
+    perc25 = quantile(q,na.rm = TRUE, probs=0.25)
+    monthlyQStats[(monthlyQStats$Month==i),c("Min","Avg","Max","Perc5","Perc25","x7Q10","x7Q2")]=c(min,avg,max,perc5,perc25,x7Q10,x7Q2)
+  }
+  return(monthlyQStats)
+}
+
+
+NWM_Stats = function(comID){
+
+  qData = read.csv('~/GitHub/Low_Flows/RetrospectiveData/RetroRecord.csv')
+  comID_column=paste0("X",comID)
+  #Calculate monthly statistics
+  qData$Month = months(as.Date(qData$Date,format="%m/%d/%Y"))
+  monthlyQStats = data.frame(Month = unique(qData$Month),Min=NA,Avg=NA,Max=NA,Perc5=NA,Perc25=NA,x7Q10=NA,x7Q2=NA)
+  #Read in discharge data
+  for (i in unique(qData$Month)){
+    q = qData[(qData$Month==i),comID_column]
+    #Use Log-Pearson Type III Distribution. See: https://pubs.usgs.gov/sir/2008/5126/section3.html and
+    #http://deq1.bse.vt.edu/sifnwiki/index.php/R_iha_7q10#PearsonDS_function_details for the details on this function
+    logq=log(q)
+    pars = PearsonDS:::pearsonIIIfitML(logq)
+    x7Q2 = exp(qpearsonIII(0.5, params = pars$par))
+    x7Q10 = exp(qpearsonIII(0.1, params = pars$par))
+    min = min(q)
+    avg = mean(q)
+    max = max(q)
+    perc5 = quantile(q,na.rm = TRUE, probs=0.05)
+    perc25 = quantile(q,na.rm = TRUE, probs=0.25)
+    monthlyQStats[(monthlyQStats$Month==i),c("Min","Avg","Max","Perc5","Perc25","x7Q10","x7Q2")]=c(min,avg,max,perc5,perc25,x7Q10,x7Q2)
+  }
+  return(monthlyQStats)
+}
+
+gageID="11383500"
+comID="8020924"
+USGS_results=USGS_Stats(gageID)
+NWM_results=NWM_Stats(comID)
+
